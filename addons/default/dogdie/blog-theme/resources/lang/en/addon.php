3fcae4f3abd225bf3a52549e9cb652e0978140a4
whuailin <whuailin@126.com>
2017-01-04 15:39:14 +0800

    add blog theme

---
+++ addons/default/dogdie/blog-theme/resources/lang/en/addon.php
@@ -0,0 +1,7 @@
+<?php
+
+return [
+    'title'       => 'Blog',
+    'name'        => 'Blog Theme',
+    'description' => ''
+];
---
+++ addons/default/dogdie/blog-theme/src/BlogTheme.php
@@ -0,0 +1,8 @@
+<?php namespace Dogdie\BlogTheme;
+
+use Anomaly\Streams\Platform\Addon\Theme\Theme;
+
+class BlogTheme extends Theme
+{
+
+}
